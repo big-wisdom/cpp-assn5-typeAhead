a97a81d02b4a74ddc21817fcbb1e90046b792eff
Eli Hermann <Eli_Hermann@hotmail.com>
2021-10-27 10:52:18 -0600

    initial commit, added all the necessary files and started the classes

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello TypeAhead" << std::endl;
+    return 0;
+}
---
+++ wordTree.hpp
@@ -0,0 +1,6 @@
+#include<iostream>
+
+void add(std::string word);
+bool find(std::string word);
+std::vector<std::string> predict(std::string partial, std::uint8_t howMany);
+std::size_t size();
